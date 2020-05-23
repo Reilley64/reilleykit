9d36e26c91e9de323050c0aab3e4fdeb448695c4
Reilley <reilleygray@gmail.com>
2020-05-23 15:24:02 +1000

    Textfield

---
+++ src/components/Textfield/Textfield.js
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import useStyles from './Textfield.styles';
+
+import useTheme from '../../hooks/useTheme';
+
+
+const Textfield = ({
+  className, disabled, field, label, required, ...props
+}) => {
+  const theme = useTheme();
+  const classes = useStyles({ disabled, theme });
+
+  return (
+    <div>
+      {label
+      && <label className={classes.label}>
+        {label}{required && <span className={classes.requiredSpan}> *</span>}
+      </label>
+      }
+      <div className={classes.wrapper}>
+        <input
+          className={clsx(classes.input, className)}
+          {...field}
+          {...props}
+          disabled={disabled}
+          type={'text'}
+          required={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Textfield;
---
+++ src/components/Textfield/Textfield.styles.js
@@ -0,0 +1,54 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  input: {
+    backgroundColor: 'transparent',
+    border: '0 none',
+    boxSizing: 'border-box',
+    color: 'inherit',
+    cursor: 'inherit',
+    fontFamily: 'inherit',
+    height: '2.6em',
+    minWidth: '0',
+    outline: 'currentcolor none medium',
+    padding: '8px 6px',
+    width: '100%',
+    '&:disabled::placeholder': {
+      color: 'rgb(165, 173, 186)',
+      opacity: '1',
+    },
+  },
+  wrapper: ({ disabled, theme }) => ({
+    alignItems: 'center',
+    backgroundColor: 'rgb(250, 251, 252)',
+    border: '2px solid rgb(233, 225, 230)',
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    cursor: 'text',
+    display: 'flex',
+    flex: '1 1 100%',
+    justifyContent: 'space-between',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    overflowWrap: 'break-word',
+    pointerEvents: 'auto',
+    transition: 'background-color .2s ease-in-out 0s, border-color .2s ease-in-out 0s',
+    verticalAlign: 'top',
+    '&:hover': {
+      backgroundColor: disabled ? 'rgb(250, 251, 252)' : 'rgb(235, 236, 240)',
+    },
+    '&:focus-within': {
+      backgroundColor: 'rgb(255, 255, 255)',
+      borderColor: theme.palette.primary.main,
+    },
+  }),
+  label: ({ theme }) => ({
+    color: theme.palette.text.muted,
+    fontSize: '.85rem',
+  }),
+  requiredSpan: ({ theme }) => ({
+    color: theme.palette.danger.main,
+  }),
+});
+
+export default useStyles;
