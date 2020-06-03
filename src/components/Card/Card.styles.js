5d00055be58ef1108a60da873e35e84ac57f428a
Reilley Gray <reilleygray@gmail.com>
2020-06-03 22:48:53 +1000

    Card styles update

--- src/components/Card/Card.styles.js
+++ src/components/Card/Card.styles.js
@@ -4,7 +4,7 @@ const useStyles = createUseStyles({
   card: {
     display: 'flex',
     flexDirection: 'column',
-    padding: '16px 0',
+    padding: '16px',
     borderRadius: '3px',
     boxShadow: 'rgba(9, 30, 66, 0.25) 0px 1px 1px, rgba(9, 30, 66, 0.31) 0px 0px 1px 0px',
   },
