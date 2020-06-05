c6addb1f75f138eafc3242f2bb5b4aeda51f5965
Reilley Gray <reilleygray@gmail.com>
2020-06-06 00:05:47 +1000

    Menu heading fixes

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -214,11 +214,11 @@ var useStyles$3 = reactJss.createUseStyles({
   menuHeader: {
     color: 'rgb(107, 119, 140)',
     textTransform: 'uppercase',
-    marginTop: '1.5rem',
-    marginBottom: '1rem',
+    marginTop: '1.5rem !important',
+    marginBottom: '1rem !important',
     padding: '0.5rem 1.25rem',
     '&:first-child': {
-      marginTop: '0'
+      marginTop: '0 !important'
     }
   }
 });
