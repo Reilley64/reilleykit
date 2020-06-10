2a1ebe9f290b65b5cb773b5e1e51ce6343892c0f
Reilley Gray <reilleygray@gmail.com>
2020-06-10 17:19:12 +1000

    Palette update

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -7,7 +7,7 @@ const ThemeProvider = ({ children }) => {
   const value = {
     palette: {
       primary: {
-        main: 'rgb(44, 62, 80)',
+        main: 'rgb(51, 94, 234)',
       },
       secondary: {
         main: 'rgb(145, 156, 167)',
