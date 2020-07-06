5e113d6507bd091d736d89dc90c5ed0a68d48092
Reilley Gray <reilleygray@gmail.com>
2020-07-06 21:44:06 +1000

    Menu fixes

--- src/components/Menu/Menu/Menu.styles.js
+++ src/components/Menu/Menu/Menu.styles.js
@@ -1,15 +1,15 @@
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
-  menu: {
-    backgroundColor: 'rgb(255, 255, 255)',
+  menu: ({ theme }) => ({
+    backgroundColor: theme.palette.card.main,
     borderRadius: '.1875rem',
     boxShadow: 'rgba(9, 30, 66, .25) 0 .0625rem .0625rem, rgba(9, 30, 66, .31) 0 0 .0625rem 0',
     overflowY: 'auto',
     willChange: 'transform',
     padding: '.375rem 0',
     width: '100%',
-  },
+  }),
 });
 
 export default useStyles;
--- src/components/Menu/MenuItem/MenuItem.styles.js
+++ src/components/Menu/MenuItem/MenuItem.styles.js
@@ -1,14 +1,14 @@
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
-  menuItem: ({ link, onClick }) => ({
-    color: 'rgb(0, 0, 0)',
+  menuItem: ({ link, onClick, theme }) => ({
+    color: theme.palette.text.base,
     display: 'flex',
     padding: '.5rem 1.25rem',
     userSelect: (link || onClick) ? 'none' : 'default',
     width: '100%',
     '&:hover': {
-      backgroundColor: (link || onClick) ? 'rgb(235, 236, 240)' : 'transparent',
+      backgroundColor: (link || onClick) ? theme.palette.interact.main : 'transparent',
     },
     '& span': {
       alignItems: 'center',
